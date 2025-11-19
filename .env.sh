2ebd4a0c21e19917137333af2c2f82769ddcd40b
nestorr2312 <aguileratorronestor@gmail.com>
2025-11-19 09:11:23 +0100

    Create .env.sh

---
+++ .env.sh
@@ -0,0 +1,10 @@
+# Archivo: scripts/.env
+
+export DB_ROOT_PASS="TU_CONTRASEÑA_ROOT_SEGURA"
+export DB_USER="usuario_lamp"
+export DB_PASS="TU_CONTRASEÑA_LAMP_USER"
+export DB_NAME="basedatos_lamp"
+
+export WWW_DIR="/var/www/html"
+export APACHE_LOGS_DIR="/var/log/apache2"
+export ADMINER_VERSION="4.8.1"
